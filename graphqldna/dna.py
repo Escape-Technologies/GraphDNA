170739a03e2fe76a1c43ae77aad23b8576023236
c3b5aw <dev@c3b5aw.dev>
2022-07-11 00:05:57 +0200

    chore: move http bucket to dedicated file

--- graphqldna/dna.py
+++ graphqldna/dna.py
@@ -3,12 +3,10 @@
 import asyncio
 import logging
 
-import aiohttp
-
 from graphqldna.entities import GraphQLEngine
 from graphqldna.entities.interfaces import IGraphQLDNA
-from graphqldna.entities.interfaces.dna import IHTTPBucket, IRequest
 from graphqldna.heuristics import HeuristicsManager
+from graphqldna.http import HTTPBucket
 from graphqldna.logger import setup_logger
 
 
@@ -30,78 +28,6 @@ async def detect_engine_async(
 
     dna = GraphQLDNA(url, headers)
     return await dna.run()
-
-
-class HTTPBucket(IHTTPBucket):
-
-    """Defines a HTTP Bucket, which store procedeed requests.
-
-    The goal is to send requests once.
-    """
-
-    def __init__(
-        self,
-        logger: logging.Logger,
-        headers: dict[str, str] | None,
-    ) -> None:
-        self._store = {}
-        self._queue = []
-        self._session = None
-
-        self._headers = headers or {}
-        self._logger = logger
-
-    async def put(
-        self,
-        req: IRequest,
-        key: str,
-    ) -> None:
-        if not self._session:
-            await self.open_session()
-
-        if key not in self._store:
-            task = asyncio.create_task(self.send_request(req, key))
-            self._store[key] = task
-            self._queue.append(task)
-
-    def get(
-        self,
-        key: str,
-    ) -> aiohttp.ClientResponse:
-        value = self._store.get(key)
-        assert isinstance(value, aiohttp.ClientResponse)
-        return value
-
-    async def send_request(
-        self,
-        request: IRequest,
-        key: str,
-    ) -> None:
-        if not self._session:
-            self._session = await self.open_session()
-
-        self._store[key] = await self._session.request(
-            request.method,
-            request.url,
-            **request.kwargs,
-        )
-
-    async def consume_bucket(self) -> None:
-        self._logger.info(f'Consuming bucket of {len(self._queue)} requests.')
-
-        for task in self._queue:
-            await task
-
-    async def open_session(self) -> aiohttp.ClientSession:
-        self._session = aiohttp.ClientSession(headers=self._headers, )
-        return self._session
-
-    async def close_session(self) -> None:
-        if not self._session:
-            return
-
-        await self._session.close()
-        self._session = None
 
 
 class GraphQLDNA(IGraphQLDNA):
