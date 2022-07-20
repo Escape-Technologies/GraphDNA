c1dbf211524ae643b5bb77353ff226b043158d37
thibaudlabat <90899718+thibaudlabat@users.noreply.github.com>
2022-07-20 16:02:17 +0200

    fix: gqlgen heuristic (#7)
    * fix: gqlgen heuristic
    
    * fix: gqlgen score
    
    Co-authored-by: thibaudlabat <thibaudlabat@gitlab.com>

--- graphdna/heuristics/gql_queries/gqlgen.py
+++ graphdna/heuristics/gql_queries/gqlgen.py
@@ -6,7 +6,9 @@ from graphdna.entities.interfaces.heuristics import IGQLQuery
 class GQLGen(IGQLQuery):
 
     score_factor = 0.63
+    score = 200
     genetics = {
-        'query { __typename {}': in_response_text('Directive \\"deprecated\\" may not be used on FIELD.'),
+        'query  { __typename {}': in_response_text('GRAPHQL_PARSE_FAILED'),
+        'query {enumInInput(input: {enum: INVALID})}': in_response_text('GRAPHQL_VALIDATION_FAILED'),
         'query { alias^_:__typename {}': in_response_text('Expected Name, found <Invalid>')
     }
