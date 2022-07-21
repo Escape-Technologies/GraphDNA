2cb3e55fadf36e17d4b7efa9d22032d018912baf
c3b5aw <spyk.developpeur@gmail.com>
2022-07-21 11:43:39 +0200

    Revert "fix: gqlgen heuristic" (#8)
    Revert "fix: gqlgen heuristic (#7)"
    
    This reverts commit c1dbf211524ae643b5bb77353ff226b043158d37.

--- graphdna/heuristics/gql_queries/gqlgen.py
+++ graphdna/heuristics/gql_queries/gqlgen.py
@@ -6,9 +6,7 @@ from graphdna.entities.interfaces.heuristics import IGQLQuery
 class GQLGen(IGQLQuery):
 
     score_factor = 0.63
-    score = 200
     genetics = {
-        'query  { __typename {}': in_response_text('GRAPHQL_PARSE_FAILED'),
-        'query {enumInInput(input: {enum: INVALID})}': in_response_text('GRAPHQL_VALIDATION_FAILED'),
+        'query { __typename {}': in_response_text('Directive \\"deprecated\\" may not be used on FIELD.'),
         'query { alias^_:__typename {}': in_response_text('Expected Name, found <Invalid>')
     }
